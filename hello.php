d53904ffe73e951a8f220a076a2a4c13ce6a353c
rabab003 <bintaliraq4@gmail.com>
2025-10-19 19:35:10 +0300

    if condition in PHP langauge

--- hello.php
+++ hello.php
@@ -1,9 +1,21 @@
 <?php
+ 
+$input = readline("Enter your grade: ");
 
-$name = "rabab";
-print "Hello $name";
-echo "  this is me ali  
-";
+$grade = $input;
 
-$students = ["rabab","noor"];
-echo $students[0];
+if($grade >=98){
+    echo "A+ Grade";
+}else if($grade >= 80){
+    echo "very good";
+}else if($grade >= 70){
+    echo "good";
+}else if($grade >= 60){
+    echo " not good";
+}
+else if($grade >= 50){
+    echo "bad";
+}else{
+    echo "F Grade";
+}
+
