76ddb104879f9c8931cb66d3ff25f9ab4dc7a491
rabab003 <bintaliraq4@gmail.com>
2025-10-21 22:10:22 +0300

    oop section

--- hello.php
+++ hello.php
@@ -1,17 +1,25 @@
 <?php
  
-$seating_list = [
-["a1","a2","a3","a4"],
-["b1","b2","b3","b4"],
-["c1","c2","c3","c4"],
-["d1","d2","d3","d4"]
-];
+//shapes 
+//square triangle, rectangle
 
 
-for($row = 0; $row <count($seating_list); $row++){
-    for($col = 0 ; $col < count($seating_list[$row]); $col++){
-        echo $seating_list[$row][$col] . "\t";
-    }
+//rotate()
+//playSound()
 
-    echo "\n";
+
+function rotate($degree){
+    $angle = 0;
+    $newAngle = $angle + $degree;
+    return $newAngle % 360 ;
+
 }
+
+function playSound(){
+    echo "playing sound";
+}
+
+
+$sqaure =0 ;
+$triangle = 0 ;
+$rectangle = 0 ;
