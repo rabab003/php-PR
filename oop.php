903c375ff6f9ae91ba72860ba0a19580fd3b4bf3
rabab003 <bintaliraq4@gmail.com>
2025-10-23 15:32:19 +0300

    OOP ends here

--- oop.php
+++ oop.php
@@ -2,20 +2,34 @@
 
 class Shape {
     private $degree =15 ;
-    private $angle = "circle";
+    protected $name = "circle";
 
+    public function __construct($degree, $angle) {
+        $this->degree = $degree;
+        $this->angle = $angle;
+    }
 
     public function rotate(){
-        echo "rotating ..." . $this->degree;
+        echo "rotating ..." . $this->degree . "\n";
     }
 
     public function playSound(){
-        echo "playing sound ..." . $this->angle;
+        echo "playing sound ..." . $this->name . "\n";
     }
 }
 
 
-$circle = new Shape();
+class square extends Shape{
+    public function __construct( ) {
+}
+
+public function playSound(){
+    echo "play sound for specific shape". $this->name;
+}
+}
+
+$circle = new Shape("cir", 45);
+$sqaure = new Shape("sq", 65);
 
 $circle->rotate();
 $circle->playSound();
